e30770483156af066bfd9497c872b1fb6ad403c1
Dinechko <m@dichenko.ru>
2022-07-05 08:38:59 +0300

    install

---
+++ ERC721/contracts/PandaNFT.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
