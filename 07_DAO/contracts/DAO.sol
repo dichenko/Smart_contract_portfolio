6b07af1efe29060fc5d3be357c83b3e580c1c23a
Dinechko <m@dichenko.ru>
2022-07-25 10:26:35 +0300

    bug

--- 07_DAO/contracts/DAO.sol
+++ 07_DAO/contracts/DAO.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+bytes32 public constant CHAIRMAN = keccak256("CHAIRMAN");
 
 contract DAO{
 
@@ -12,13 +14,21 @@ contract DAO{
     mapping (address => uint) deposit;
 
     Voting {
+        bool finished;
         uint96 startTime;
         address initiator;
         address recipient;
-        
-
+        bytes4 signature;
+        uint[2] votesCounter; 
     }
+
+    Voting[] votings;
 
     event VotingStarted(uint id, uint starttime, address recipient, bytes4 signature);
     event VotingFinished(uint id, uint optionID);
+
+    constructor (address _chairman){
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(CHAIRMAN, _chairman);
+    }
 }
