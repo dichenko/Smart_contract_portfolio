efd10157cb19108c29166e1f70fbf807382d8bf6
Dinechko <m@dichenko.ru>
2022-07-28 15:26:21 +0300

    deploy

--- 07_DAO/deploy/full-deploy.ts
+++ 07_DAO/deploy/full-deploy.ts
@@ -1,21 +1,36 @@
+
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 
 async function main() {
+  
+  let dao: Contract;
+  let erc20: Contract;
   let staking: Contract;
   let deployer: SignerWithAddress;
 
   [deployer] = await ethers.getSigners();
+  
+  //erc20 deploy
+  const MyERC20Factory = await ethers.getContractFactory("GovernanceToken");
+  erc20 = await MyERC20Factory.deploy();
+  erc20.deployed();
+  console.log("ERC20 deployed to:", erc20.address);
+
 
   //staking deploy
   const StakingFactory = await ethers.getContractFactory("Staking");
   staking = await StakingFactory.deploy();
   staking.deployed();
   console.log("Staking deployed to:", staking.address);
-  console.log("grant role Dao");
-  await staking.grantRole(staking.DAO(), process.env.DAO_ADDRESS as string);
-  console.log("Dao granted");
+
+
+   //DAO deploy
+   const DAOFactory = await ethers.getContractFactory("DAO");
+   dao = await DAOFactory.deploy(erc20.address);
+   dao.deployed();
+   console.log("DAO deployed to:", dao.address);
 }
 
 main()
